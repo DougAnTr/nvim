03626a969d4d8bb38b7eb1785c99239ca5f1401b
Douglas Trofino <douglast@outboxup.com>
2025-02-15 13:05:06 -0300

    changes to make it work on Ubuntu WSL

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,13 +1,11 @@
-return {}
-
--- return {
---   'zbirenbaum/copilot.lua',
---   cmd = 'Copilot',
---   lazy = true,
---   config = function()
---     require('copilot').setup {
---       suggestion = { enabled = false },
---       panel = { enabled = false },
---     }
---   end,
--- }
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  lazy = true,
+  config = function()
+    require('copilot').setup {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    }
+  end,
+}
